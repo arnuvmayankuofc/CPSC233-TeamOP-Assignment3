66ee5dae9c0199773fe4c7717dcd614d96eba102
pi-sister <67890664+pi-sister@users.noreply.github.com>
2020-08-08 17:57:11 -0600

    Added PollListException and modified PollList
    PollListFullException.java was created as a child class of java.lang.exception. All constructors were added, commenting needs to be ammended as it is copy-pasted from Exception. PollList was modified to have more professional comments and to have addPoll throw PollListFullException.

--- src/model/PollList.java
+++ src/model/PollList.java
@@ -1,93 +1,127 @@
 package model;
 
-/** 
- * The PollList Class has two instance variables: 
- * 1) a list of polls stored in an array.
- * 2) the number of seats available for the election.
+import java.util.ArrayList;
+
+/**
+ * The PollList Class has two instance variables: 1) a list of polls stored in
+ * an array. 2) the number of seats available for the election.
  * 
-
+ * 
  * @author Jamie MacDonald
- * @since July 16 2020
- 
- * */
-
+ * @since 1.0
+ * 
+ */
 public class PollList {
 	private Poll[] polls;
 	private int numOfSeats;
-	
-	/* PollList constructor takes the number of Polls and the number of seats as arguments.
-	 * It checks that the # of Polls and seats is above 1, otherwise sets default numbers.
-	 * The num of Polls sets the length of the array Poll
-	 * The num of Seats sets the current method 
-	 * */
+
+	/**
+	 * Constructs a new PollList with a specified number of Polls and number of
+	 * Seats. It checks that the # of Polls and seats is above 1, otherwise sets
+	 * default numbers.
+	 * 
+	 * @param numOP the number of Polls to be included.
+	 * @param numOS the number of Seats in the election
+	 * 
+	 * @since 1.0
+	 */
 	public PollList(int numOP, int numOS) {
-		if (numOP < 1) numOP = 5;
-		if (numOS < 1) numOS = 10;
+		if (numOP < 1)
+			numOP = 5;
+		if (numOS < 1)
+			numOS = 10;
 		polls = new Poll[numOP];
 		this.numOfSeats = numOS;
 	}
-	
-	// getter for instance variable numOfSeats
+
+	/**
+	 * Fetches the number of seats in the invoked PollList.
+	 * 
+	 * @return The number of seats.
+	 * @since 1.0
+	 */
 	public int getNumOfSeats() {
 		return numOfSeats;
 	}
-	
-	// getter for instance variable polls
+
+	/**
+	 * Fetches the list of Polls in the invoked PollList.
+	 * 
+	 * @return the array containing the list of polls.
+	 * @since 1.0
+	 */
 	public Poll[] getPolls() {
 		return polls;
 	}
-	
-	/* addPoll method takes a Poll as an argument, and completes the following checks:
-	 * 1) the Poll submitted is not equivalent to null, 
-	 * 2) the Poll name does not already exist within the poll list (if so it replaces the name)
-	 * 3) the poll can be added to the end of the list or the list is full 
-	 * */
-	public void addPoll(Poll aPoll) {
+
+	/**
+	 * Completes the following checks on a Poll before adding to the invoked
+	 * PollList:
+	 * <li>1) the Poll submitted is not equivalent to null,
+	 * <li>2) the Poll name does not already exist within the poll list (if so it
+	 * replaces the name)
+	 * <li>3) the poll can be added to the end of the list or the list is full <br>
+	 * 
+	 * @param aPoll the {@code Poll} to be added to this list.
+	 * @since 1.0
+	 */
+	public void addPoll(Poll aPoll) throws PollListFullException {
 		if (aPoll == null) {
 			System.out.print("Error. Please provide the poll name.");
 		}
-		
+
 		else {
 			int i = 0;
 			Boolean exist = false;
-			for (; i < polls.length && polls[i] != null; i++) { 
-				if ((polls[i].getPollName()).equalsIgnoreCase(aPoll.getPollName())) { 
+			for (; i < polls.length && polls[i] != null; i++) {
+				if ((polls[i].getPollName()).equalsIgnoreCase(aPoll.getPollName())) {
 					polls[i] = aPoll;
 					exist = true;
-				} 
+				}
 			}
 			if (i < polls.length) {
-					polls[i] = aPoll;
-					exist = true;
-			}
-			else {
-			System.out.print("The list is full. No further polls can be added.");
+				polls[i] = aPoll;
+				exist = true;
+			} else {
+				throw new PollListFullException();
 			}
 		}
 	}
-	
-	/* Takes an array of the party names as an argument. For every party named, the average party data is 
-	 * retrieved and stored in an Aggregate Poll. The poll is then normalized and returned.
+
+	/**
+	 * For each party named in a list, the average party data is retrieved and
+	 * stored in an Aggregate Poll. The poll is then normalized.
+	 * 
+	 * @param partyNames the array list of party names.
+	 * @return A {@code Poll} containing the named Parties with averaged data.
+	 * 
+	 * @since 1.0
 	 */
 	public Poll getAggregatePoll(String[] partyNames) {
 		Poll Aggregate = new Poll("Aggregate", partyNames.length);
 		for (String p : partyNames) {
 			Party avedParty = getAveragePartyData(p);
-			Aggregate.addParty(avedParty);			
+			Aggregate.addParty(avedParty);
 		}
 		adjustPollToMaximums(Aggregate);
 		return Aggregate;
 	}
-	
-	/* Takes a Party name as an argument, then loops through all polls in the poll list to calculate average
-	 * number of seats and average percentage of votes. Returns Party with averages.
+
+	/**
+	 * Loops through all polls in the {@code PollList} to calculate average number
+	 * of seats and average percentage of votes for a given {@code Party}.f
+	 * 
+	 * @param partyName a {@code Party} to be retrieved and averaged.
+	 * @return {@code Party} with average data.
+	 * 
+	 * @since 1.0
 	 */
 	public Party getAveragePartyData(String partyName) {
 		Party aParty = new Party(partyName);
 		int pollCount = 0;
 		float seatCount = 0;
 		float percCount = 0;
-		for (Poll i: polls) {
+		for (Poll i : polls) {
 			Party p = i.getParty(partyName);
 			if (p != null) {
 				seatCount += p.getProjectedNumberOfSeats();
@@ -96,18 +130,23 @@ public class PollList {
 			}
 		}
 		if (pollCount > 0) {
-			aParty.setProjectedNumberOfSeats(seatCount/pollCount);
-			aParty.setProjectedPercentageOfVotes(percCount/pollCount);
+			aParty.setProjectedNumberOfSeats(seatCount / pollCount);
+			aParty.setProjectedPercentageOfVotes(percCount / pollCount);
 		}
 		return aParty;
 	}
-	
-	/* Takes a Poll as an argument. The number of seats and percentage of votes is summed from all the parties
-	 * in the Poll. If either number is above a logical maximum, the data of each Party is corrected.
-	 * Returns the corrected Poll. 
+
+	/**
+	 * Sums the number of seats and percentage of votes from all the parties in a
+	 * given {@code Poll}. If either number is above a logical maximum, the data of
+	 * each {@code Party} is corrected.
+	 * 
+	 * @param aPoll the {@code Poll} to be corrected.
+	 * @return {@code Poll} with corrected seats and vote percentage.
+	 * @since 1.0
 	 */
 	public Poll adjustPollToMaximums(Poll aPoll) {
-		Party [] aParties = aPoll.getPartiesSortedBySeats();
+		Party[] aParties = aPoll.getPartiesSortedBySeats();
 		float seatCheck = 0;
 		float percCheck = 0;
 		for (Party a : aParties) {
@@ -116,40 +155,41 @@ public class PollList {
 		}
 		if (seatCheck > this.numOfSeats) {
 			for (Party p : aParties) {
-				float fractionOfSeats = p.getProjectedNumberOfSeats()/seatCheck;
-				p.setProjectedNumberOfSeats(fractionOfSeats*this.numOfSeats);
+				float fractionOfSeats = p.getProjectedNumberOfSeats() / seatCheck;
+				p.setProjectedNumberOfSeats(fractionOfSeats * this.numOfSeats);
 			}
 		}
 		if (percCheck > 1.0) {
 			for (Party j : aParties) {
-				float fractionOfVotes = j.getProjectedPercentageOfVotes()/percCheck;
+				float fractionOfVotes = j.getProjectedPercentageOfVotes() / percCheck;
 				j.setProjectedPercentageOfVotes(fractionOfVotes);
 			}
 		}
 		return aPoll;
 	}
-	
-	/* Cycles through all Polls in Poll List and makes a new poll with all the party names. Party names are then 
-	 * stored in an array of strings, which is returned.
+
+	/**
+	 * Cycles through all Polls in the {@code PollList} and collects all novel Party
+	 * names to an ArrayList.
+	 * 
+	 * @return Array of all Party names in all Polls.
+	 * @since 2.0
 	 */
 	public String[] getPartyNames() {
-		Poll allPossible = new Poll("allPossible", 30);
+		ArrayList<String> allPartyNames = new ArrayList<String>();
 		for (Poll a : polls) {
 			for (Party b : a.getPartiesSortedBySeats()) {
-				allPossible.addParty(b);
+				if (!allPartyNames.contains(b.getName())) {
+					allPartyNames.add(b.getName());
+				}
 			}
 		}
-		String[] allPartyNames = new String[30];
-		Party[] allParties = allPossible.getPartiesSortedBySeats();
-		for (int i = 0; i < allParties.length && allParties[i] != null; i++) {
-			allPartyNames[i] = allParties[i].getName();
-		}
-		return allPartyNames;
+		return (String[]) allPartyNames.toArray();
 	}
-	
+
 	// Overrides the toString to return a string of the number of seats.
 	@Override
 	public String toString() {
-		return "Number of seats: "+ this.numOfSeats;
+		return "Number of seats: " + this.numOfSeats;
 	}
 }
